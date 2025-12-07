29446ed77519d9c96eef8c1b7b461244303288ea
Amanda Ogletree <amandaogletree19@gmail.com>
2025-12-07 13:58:23 -0500

    Added styling to Stats Display. Updated measurement component for border radius from vw to rem for better responsive design.

--- src/components/StatsDisplay/StatsDisplay.tsx
+++ src/components/StatsDisplay/StatsDisplay.tsx
@@ -1,3 +1,4 @@
+import './StatsDisplay.css'
 import type { StatsDisplayProps } from "../../type";
 
 
@@ -5,18 +6,18 @@ export default function StatsDisplay({stats, showReadingTime}:StatsDisplayProps)
 
     return (
         <>
-            <div>
-                <div>
+            <div className='stats-container'>
+                <div className='stat-container-character-count'>
                     <p className="stats-title">Characters</p>
                     <p className="stats-number">{stats.characterCount}</p>
                 </div>
                 <br/>
-                <div>
+                <div className='stat-container-word-count'>
                     <p className="stats-title">Words</p>
                     <p className="stats-number">{stats.wordCount}</p>
                 </div>
                 <br/>
-                <div hidden={!showReadingTime}>
+                <div className='stat-container-reading-time' hidden={!showReadingTime}>
                     <p className="stats-title">Reading Time</p>
                     <p className="stats-number">{stats.readingTime}ms</p>
                 </div>
